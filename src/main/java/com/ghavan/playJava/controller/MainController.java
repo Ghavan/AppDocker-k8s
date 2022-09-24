3d4fc65a75149e7966c7943d9746b5968202d5e5
Ghavan <ghavan@gmail.com>
2022-09-23 23:30:07 -0400

    Setup is done

---
+++ src/main/java/com/ghavan/playJava/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.ghavan.playJava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/test-docker")
+	public String getData() {
+		return "in docker tutorial project of play java";
+		
+	}
+	
+}
